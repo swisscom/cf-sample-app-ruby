8b544ff6689d724633207f100a04f5b1b37b8987
Tobias Fuhrimann <tobias.fuhrimann@swisscom.com>
2016-04-11 23:51:17 +0200

    add comments

--- app.rb
+++ app.rb
@@ -1,14 +1,18 @@
+# require dependencies
 Bundler.require :web
 Bundler.require :development if development?
 
+# serve stylesheet
 get '/style.css' do
   scss :style
 end
 
+# our basic route which renders the index view
 get '/' do
   haml :index
 end
 
+# our not-found route which serves the 404 view
 not_found do
   haml :'404'
 end
